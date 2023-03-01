7fe5dfd5c8d72dac493c8ce8b8a61b91fa92f283
satyam472 <satyamdubeyshd91@gmail.com>
2023-03-01 10:05:35 +0530

    handle form and table clicks

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -3,15 +3,20 @@ import '../css/Home.css'
 import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom';
 const Home = () => {
-    
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors }
-  } = useForm();
 
-    const [crossOver, setcrossOver] = useState([]);
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: { errors }
+    } = useForm();
+
+    const [crossOver, setcrossOver] = useState();
+    const [formStatus, setformStatus] = useState(true);
+    const [tableStatus, settableStatus] = useState(false);
+    const [EmaShort, setEmaShort] = useState(false);
+    const [EmaLong, setEmaLong] = useState(false);
+    const [CandleLength, setCandleLength] = useState(false);
 
     // const fetchData = async () => {
 
@@ -21,29 +26,40 @@ const Home = () => {
     //     setcrossOver(tempdata);
     // }
     const [formData, setFormData] = useState({
-        emaLine1:"",
-        emaLine2:""
-      })
-    
+        EmaShort: "",
+        EmaLong: "",
+        CandleLength:""
+    })
+
     //   const navigate = useNavigate();
-      const onSubmit = async(body) => {
+    const onSubmit = async (body) => {
+        setformStatus(false)
+        settableStatus(true)
+        setEmaShort(body.EmaShort)
+        setEmaLong(body.EmaLong)
+        setCandleLength(body.CandleLength)
         let userDetail = {
-            emaLine1: body.emaLine1,
-            emaLine2: body.emaLine2
+            EmaShort: body.EmaShort,
+            EmaLong: body.EmaLong,
+            CandleLength: body.CandleLength
         }
         console.log(userDetail);
         const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(userDetail)
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userDetail)
         };
-        const response = await fetch("http://127.0.0.1:5000/getCrossOver",requestOptions);
+        const response = await fetch("http://127.0.0.1:5000/api/data", requestOptions);
         const tempdata = await response.json();
         console.log(tempdata);
         setcrossOver(tempdata);
         reset();
-      };
+    };
 
+    const getAnotherCrossoer = ()=>{
+        setformStatus(true)
+        settableStatus(false)
+    }
     useEffect(() => {
         // Fetch data initially
         // fetchData();
@@ -57,28 +73,48 @@ const Home = () => {
 
     return (
         <>
+
             <div className="container ">
-                <div className="row">
-                    <div className=" col-12 col-md-12 d-flex justify-content-center align-items-center">
-                        <form className="form mt-4" onSubmit={handleSubmit(onSubmit)}>
-                            <div className="form-control">
-                                <label>EmaLine1</label>
-                                <input type="number" name="emaLine1" placeholder="10" {...register("emaLine1")} />
+                {
+                    formStatus ? (<div className="row">
+                        <div className=" col-12 col-md-12 d-flex justify-content-center align-items-center">
+                            <form className="form mt-4" onSubmit={handleSubmit(onSubmit)}>
+                                <div className="form-control">
+                                    <label>EmaShort</label>
+                                    <input type="number" name="EmaShort" placeholder="10" {...register("EmaShort")} />
+                                </div>
+                                <div className="form-control">
+                                    <label>EmaLong</label>
+                                    <input type="number" name="EmaLong" placeholder="20" {...register("EmaLong")} />
+                                </div>
+                                <div className="form-control">
+                                    <label>Candle Length</label>
+                                    <input type="number" name="candleLength" placeholder="3" {...register("CandleLength")} />
+                                </div>
+                                <div className="form-control form-button">
+                                    <label></label>
+                                    <button type="submit" className="submit-button" >Get Crossover</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>) :""
+                }
+                {
+                    tableStatus? (<div className="container form-container">
+                    <div className="row heading d-flex justify-content-center allign-items-center">
+                        <div className="row">
+                            <div className="col col-md-6">
+                            <h3>Following data is based where  <br />EmaShort ={EmaShort}  <br />EmaLong ={EmaLong}  <br /> candleLength = {CandleLength}</h3>
                             </div>
-                            <div className="form-control">
-                                <label>EmaLine2</label>
-                                <input type="number" name="emaLine2" placeholder="20" {...register("emaLine2")} />
+                            <div className="col col-md-6 ">
+                            <div className="form-control form-button d-flex justify-content-center allign-items-center">
+                                    <label></label>
+                                    <button onClick={getAnotherCrossoer} className="submit-button" >Get another crossover</button>
+                                </div>
                             </div>
-                            <div className="form-control form-button">
-                                <label></label>
-                                <button type="submit" className="submit-button" >Get Crossover</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div className="container form-container">
-                    <div className=" heading d-flex justify-content-center allign-items-center">
-                        <h3>TimeStamp and Price at each CrossOver</h3>
+                        </div>
+                        
+                        <h3 className="d-flex justify-content-center allign-items-center">TimeStamp and Price at each CrossOver</h3>
                     </div>
                     <div className="container-top">
                         <table class="table">
@@ -90,7 +126,7 @@ const Home = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                {
+                                {/* {
                                     crossOver.map((detail, index) => {
                                         <tr>
                                             <th scope="row">{index + 1}</th>
@@ -98,7 +134,7 @@ const Home = () => {
                                             <td>{detail.Price}</td>
                                         </tr>
                                     })
-                                }
+                                } */}
                                 <tr>
                                     <th scope="row">2</th>
                                     <td>Jacob</td>
@@ -122,7 +158,8 @@ const Home = () => {
                             </tbody>
                         </table>
                     </div>
-                </div>
+                </div>):""
+                }
             </div>
         </>
     );
